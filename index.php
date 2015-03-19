664a5ae22fe44d002e6dbc35fc719f2e0808f089
ronakrrb <rb@pykih.com>
2015-03-19 21:31:20 +0530

    A sample usecase for IRCTC form

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+body {
+	margin-left: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	background-color: #3C9;
+}
+</style>
+</head>
+
+<body>
+<table width="1006" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <th scope="col"><table width="1280" border="0" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+        <th colspan="10" align="center" valign="middle" bgcolor="#FFFFFF" scope="col">&nbsp;</th>
+        </tr>
+      <tr>
+        <td colspan="10" align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        </tr>
+      <tr>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+      </tr>
+      <tr>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+      </tr>
+      <tr>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+      </tr>
+      <tr>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+        <td align="center" valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+      </tr>
+    </table></th>
+  </tr>
+</table>
+</body>
+</html>
